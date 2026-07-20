7764e65cbec34389c90200f86f11a47b9fb33e96
Chris <88351523+Cwong23@users.noreply.github.com>
2026-07-20 14:03:40 -0700

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+
+        def create_str_pos(col: int) -> str:
+            res = "." * (n - (n - col)) + "Q" + "." * (n - col - 1)
+            return res
+
+        res = []
+        spots = []
+
+        def dfs(row: int):
+            for col in range(n):
+                if col not in cols and (row+col) not in pos_diag and (row-col) not in neg_diag:
+                    if row + 1 == n:
+                        temp = []
+                        spots.append(col)
+                        for x in spots:
+                            temp.append(create_str_pos(x))
+                        res.append(temp)
+                        spots.pop()
+                    else:
+                        cols.add(col)
+                        pos_diag.add(row+col)
+                        neg_diag.add(row-col)
+                        spots.append(col)
+                        dfs(row+1)
+                        spots.pop()
+                        cols.remove(col)
+                        pos_diag.remove(row+col)
+                        neg_diag.remove(row-col)
+
+        dfs(0)
+
+        return res
