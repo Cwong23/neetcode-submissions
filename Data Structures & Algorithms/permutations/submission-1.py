df57818bef7aae8d189a12562781fc1d34990f5e
Chris <88351523+Cwong23@users.noreply.github.com>
2026-07-20 15:31:39 -0700

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # o(n! * n)
+        res = []
+        curr_values = set()
+
+        def backtrack(combo):
+            if len(combo) == len(nums):
+                res.append(combo[:])
+                return
+            for x in nums:
+                if x not in curr_values:
+                    curr_values.add(x)
+                    combo.append(x)
+                    backtrack(combo)
+                    combo.pop()
+                    curr_values.remove(x)
+            return
+        
+        backtrack([])
+
+        return res
